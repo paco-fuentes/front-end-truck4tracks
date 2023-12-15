b5fb87fe2892c4e92f6d4fbaa69b4a9d5f2f85ee
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-15 10:12:05 +0100

    feat(router): wrap app component with react-router-dom

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -3,17 +3,19 @@ import axios from "axios";
 
 // user
 export const register = async (body) => {
-  return await axios.post(process.env.HOST_URL`/user/register`, body);
+  return await axios.post(`http://localhost:4000/api/user/register`, body);
 };
 
 export const login = async (body) => {
-  return await axios.post(process.env.HOST_URL`/user/login`, body);
+  return await axios.post(`http://localhost:4000/api/user/login`, body);
 };
 
 export const userProfile = async (token) => {
-  return await axios.get(process.env.HOST_URL`/user/profile`, {
+  return await axios.get(`http://localhost:4000/api/user/profile`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 };
+
+// process.env.HOST_URL
