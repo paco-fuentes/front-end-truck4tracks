8fde4fdf2c5c45f04c62882bc8706ce8ce5e26fd
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-16 17:18:24 +0100

    update(role): refactor the way that token being decoded and role stored to prevent malfunctions when removing localstorage and redux

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -8,34 +8,33 @@ import { jwtDecode } from 'jwt-decode';
 
 
 export const Header = () => {
-
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const userCredentialsRedux = useSelector(userData);
     const token = userCredentialsRedux.credentials;
-    console.log(token);
+    let currentRole;
 
-    if (token) {
-        const decToken = jwtDecode(token);
-        console.log(decToken.role);
+    if ((typeof token) !== "object") {
+        const decToken = token ? jwtDecode(token) : null;
+        // console.log(decToken?.role);
+        // ? ---> cuando esté disponible el .role
+        currentRole = decToken?.role;
     }
+    // console.log('user role actual ---> ' + currentRole);
+
     const logOutMe = () => {
         dispatch(logout({ credentials: "" }))
         // navigate("/")
     }
 
-
     return (
         <div className="headerDesign txtHead">
             <NavButton path={"/"} title={"Home"} />
-
-
             <>
                 <NavButton path={"/login"} title={"Login"} />
                 <NavButton path={"/register"} title={"Regístrate"} />
 
             </>
-
             <>
                 <NavButton path={"/profile"} title={"Profile"} />
 
@@ -43,8 +42,6 @@ export const Header = () => {
                     <NavButton path={"/"} title={"Log Out"} />
                 </div>
             </>
-
-
         </div>
     );
 };
