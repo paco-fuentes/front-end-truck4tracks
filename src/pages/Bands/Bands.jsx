14b3a768437f8eb88a970ed9574ea02a6025067d
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-17 20:00:14 +0100

    feat(Bands): add get all bands view

--- src/pages/Bands/Bands.jsx
+++ src/pages/Bands/Bands.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
+import { getAllBands } from "../../services/apiCalls";
 import "./Bands.css";
 
 export const Bands = () => {
 
+    const [bands, setBands] = useState([])
 
+    useEffect(() => {
+        if (bands.length === 0) {
+            getAllBands()
+                .then((response) => {
+                    const bandsData = response.data.data;
+                    setBands(bandsData);
+                })
+                .catch ((error) => console.log(error));
+}
+    }, [bands]);
 
-    return (
-        <>
-            All Bands:
-        </>
-    );
+return (
+    <div>
+            {bands.length > 0 ? (
+                bands.map((band) => (
+                    <div key={band.id}>
+                        <div>Image: <img src={band.img_url} width="100"/></div>
+                        <p><strong>Band name: {band.band_name}</strong></p>
+                        <p>Band Leader: {band.band_leader.username}</p>
+                        <p>Genres: {band.genre_names[0]} / {band.genre_names[1]} / {band.genre_names[2]}</p>
+                        {/* <p>About: {band.about}</p> */}
+                        <p>Band hiring?: {band.hiring === "hiring" ? "We are hiring!" : "complete band until further notice"}</p>
+                        {/* <p>Band state: {band.is_active ? "Banda en activo" : "Banda separada"}</p> */}
+                    </div>
+                ))
+            ) : (
+                bands.length === 0 && <p>Loading appointments...</p>
+            )}
+        </div>
+);
 };
 
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const HOST_URL = `http://localhost:4000/api`;
 
+// guest API calls
+
+export const getAllBands = async () => {
+  return await axios.get(`${HOST_URL}/band/all`);
+};
+
 // user API calls
 export const registerCall = async (body) => {
   return await axios.post(`${HOST_URL}/user/register`, body);
