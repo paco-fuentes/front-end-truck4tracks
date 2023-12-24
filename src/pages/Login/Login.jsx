1ebd066e83ef0215a58e41350b2aa1e79d947afd
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-24 20:02:51 +0100

    update(css): add css on login page, update css header

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,15 @@
+import "./Login.css";
 import { useState } from "react";
 import { FieldInput } from "../../common/FieldInput/FieldInput";
 import { loginCall } from "../../services/apiCalls";
 import { useDispatch } from "react-redux";
 import { login } from "../userTokenSlice";
-import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
     // guardo la accion de dispatch en una const
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     // defino mis datos, un objeto, en el estado del hook
     const [loginBody, setLoginBody] = useState({
@@ -32,6 +34,7 @@ export const Login = () => {
                 const currentToken = result.data.token;
                 console.log(currentToken);
                 dispatch(login({ credentials: currentToken }));
+                navigate('/');
             })
             .catch(error => {
                 console.log(error.message);
@@ -40,9 +43,9 @@ export const Login = () => {
 
     return (
         <div className="loginDesign">
-            <div>
+            <div className="loginContainer">
                 <FieldInput
-                    design={''}
+                    design={'inputLogin'}
                     type={"email"}
                     name={"email"}
                     placeholder={"Enter email"}
@@ -50,13 +53,13 @@ export const Login = () => {
                 />
                 <div className='errorMsg'>{''}</div>
                 <FieldInput
-                    design={''}
+                    design={'inputLogin'}
                     type={"password"}
                     name={"password"}
                     placeholder={"Enter password"}
                     functionProp={loginBodyHandler}
                 />
-                <div onClick={loginButton}>Login</div>
+                <div className="loginButton" onClick={loginButton}>Login</div>
             </div>
         </div>
     );
