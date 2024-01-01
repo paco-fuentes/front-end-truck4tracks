9a958914ad250ab66753b7937409a311c67124f0
Paco Fuentes <pacofuentes.work@gmail.com>
2024-01-01 22:01:04 +0100

    styles(Multitrack): add code format and CSS styles

--- src/common/Multitrack/Multitrack.jsx
+++ src/common/Multitrack/Multitrack.jsx
@@ -6,19 +6,20 @@ export const Multitrack = ({ id, title, img, tracks }) => {
         <div key={id} className="multitrackCard">
             <div className="multiInfo">
                 <img src={img} alt="Band Image" />
-                <p><strong>Reel: {title}</strong></p>
+                <p><strong>{title}</strong></p>
             </div>
-            {/* {((tracks.length<0)) ? (<div className="emptyMulti">hola</div>) : ('')} */}
-            {tracks && Array.isArray(tracks) && tracks.map((track, id) => (
-                <div key={id} className="playerDesign">
-                    <div><img src={track.img_url} width="50" alt="Band Image" className="imgTrackMulti" /></div>
-                    <div className="trackTitle">{track.track_name}</div>
-                    <audio controls>
-                        <source src={track.track_url} type="audio/ogg" />
-                        Audio player not supported...
-                    </audio>
-                </div>
-            ))}
+            <div className="scrollMultiCont">
+                {tracks && Array.isArray(tracks) && tracks.map((track, id) => (
+                    <div key={id} className="playerDesign">
+                        <div><img src={track.img_url} width="50" alt="Band Image" className="imgTrackMulti" /></div>
+                        <div className="trackTitle">{track.track_name}</div>
+                        <audio controls>
+                            <source src={track.track_url} type="audio/ogg" />
+                            Audio player not supported...
+                        </audio>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
